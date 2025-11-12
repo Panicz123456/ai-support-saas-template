645467855f923be570d78a327f7a4317f4218314
patryk <paniczgg@gmail.com>
2025-11-12 16:52:33 +0100

    clerk authentication setup

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google"
 
-import { Providers } from "@/components/providers"
+import { ClerkProvider } from '@clerk/nextjs';
+import { ConvexClientProvider } from '@/components/providers';
 
 import "@workspace/ui/globals.css"
 
@@ -20,12 +21,16 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body
-        className={`${fontSans.variable} ${fontMono.variable} font-sans antialiased `}
-      >
-        <Providers>{children}</Providers>
-      </body>
-    </html>
-  )
+		<html lang="en" suppressHydrationWarning>
+			<body
+				className={`${fontSans.variable} ${fontMono.variable} font-sans antialiased `}
+			>
+				<ClerkProvider>
+          <ConvexClientProvider>
+            {children}
+          </ConvexClientProvider>
+				</ClerkProvider>
+			</body>
+		</html>
+	);
 }
